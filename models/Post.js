d0b01e6b073bc91caacf4c2333a8c7c8716081d4
Asma ben mahmoud <asma.bm@oyez.fr>
2019-06-07 18:30:20 +0200

    add post model

--- models/Post.js
+++ models/Post.js
@@ -1,13 +1,12 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-
-const PostSchema = new Shema({
+const PostSchema = new Schema({
   user: {
-    type: Schema.Types.ObjectId;
+    type: Schema.Types.ObjectId,
     ref: 'users'
   },
-  tesxt: {
+  text: {
     type: String,
     required: true
   },
@@ -15,32 +14,36 @@ const PostSchema = new Shema({
     type: String
   },
   avatar: {
-   type: String
+    type: String
   },
   likes: [
-    user: {
-      type: Schema.Types.ObjectId,
-      ref: 'users'
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      }
     }
   ],
   comments: [
-    user: {
-      type: Schema.Types.ObjectId,
-      ref: 'users'
-    },
-    text: {
-      type: String,
-      required: true 
-    },
-    name: {
-      type: String
-    },
-    avatar: {
-      type: string
-    },
-    date: {
-      type: DataCue,
-      default: Date.now
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+      },
+      text: {
+        type: String,
+        required: true
+      },
+      name: {
+        type: String
+      },
+      avatar: {
+        type: String
+      },
+      date: {
+        type: Date,
+        default: Date.now
+      }
     }
   ],
   date: {
@@ -49,4 +52,4 @@ const PostSchema = new Shema({
   }
 });
 
-module.exports = PPost = mongoose.model('post', PostSchema);
+module.exports = Post = mongoose.model('post', PostSchema);
